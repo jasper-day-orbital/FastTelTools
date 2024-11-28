eca1434e86a7267f10549f4452c3fa98f295e274
Jasper Day <jromanoday@gmail.com>
2024-11-28 17:40:34 +0000

    Add release profile

--- profiling/profile.py
+++ profiling/profile.py
@@ -23,7 +23,6 @@ with Serafin.Read(DATA_PATH, "fr") as resin:
     ikle = resin.header.ikle_2d - 1
     mesh = fastteltools.PyMesh2D(x, y, ikle)
     # oldmesh = MeshInterpolator(resin.header, True)
-    # plt.scatter(mesh.points[:, 0], mesh.points[:, 1], c="r", s=0.5)
     # plt.scatter(oldmesh.points[:, 0], oldmesh.points[:, 1], c="k", s=0.5)
     # plt.scatter(x, y, s=0.5, c="b")
     # plt.show()
@@ -33,6 +32,8 @@ with Serafin.Read(DATA_PATH, "fr") as resin:
 
     # vs = resin.read_vars_in_frame(0, var_IDs=resin.header.var_IDs)
     values = resin.read_var_in_frame(0, "B")
+    # U = resin.read_var_in_frame(0, "U")
+    # V = resin.read_var_in_frame(0, "V")
     # vs is an 8x1035 ndarray
     # print(resin.header.var_IDs)
     # ['U', 'V', 'H', 'S', 'B', 'F', 'Q', 'M']
@@ -43,12 +44,9 @@ bounds = [np.min(xm), np.max(xm), np.min(ym), np.max(ym)]
 xx = np.linspace(bounds[0], bounds[1], 200)
 yy = np.linspace(bounds[2], bounds[3], 200)
 points = np.array([[x, y] for x in xx for y in yy])
-print("npoints: ", len(points))
 
 t0 = time.perf_counter()
 point_interpolators = mesh.get_point_interpolators(points)
-print("ninterp: ", len(point_interpolators))
-# print(point_interpolators)
 t1 = time.perf_counter()
 
 print(f"Interpolated points in {(t1-t0) * 1000} ms")
@@ -62,17 +60,27 @@ print(
 
 
 t0 = time.perf_counter()
-results = []
-for point_interpolator in point_interpolators:
-    if point_interpolator is not None:
-        i, j, k = point_interpolator.indices
-        interpolator = point_interpolator.coords
-        results.append(interpolator.dot(values[[i, j, k]]))
-    else:
-        results.append(np.nan)
+
+
+def interpolate(values):
+    results = []
+    for point_interpolator in point_interpolators:
+        if point_interpolator is not None:
+            i, j, k = point_interpolator.indices
+            interpolator = point_interpolator.coords
+            results.append(interpolator.dot(values[[i, j, k]]))
+        else:
+            results.append(np.nan)
+    return results
+
+
+results = interpolate(values)
 t1 = time.perf_counter()
 
 print(f"Found values in {(t1 - t0) * 1000} ms")
 
-plt.imshow(np.array(results).reshape([200, 200]).T, extent=bounds)
-plt.show()
+# plt.imshow(np.array(results).reshape([200, 200]).T, extent=bounds, origin="lower")
+
+# plt.scatter(mesh.points[0, :], mesh.points[1, :], c="r", s=1)
+# plt.quiver(xm, ym, U, V, color="white")
+# plt.show()
